a97ab30434e292aeb440dd6cec0c60e8785b818c
Jorge Ramos <jorgeramos2068@gmail.com>
2021-01-19 21:26:46 -0700

    Initial commit

---
+++ src/app/components/usuario/usuario.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html'
+})
+export class UsuarioComponent implements OnInit
+{
+  constructor( private route: ActivatedRoute ) {
+    this.route.params.subscribe( params => {
+      console.log( 'Ruta Padre', params )
+    } );
+  }
+
+  ngOnInit() {
+  }
+}
